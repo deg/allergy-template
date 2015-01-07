d32545dfa68af028e60aa4f943a2cda9521f82b5
David Goldfarb <deg@degel.com>
2015-01-07 15:57:31 +0200

    Cleanup the template, and bump version to v0.1.0. This was mostly moving files into proper directories. Really, should fix lein-create-template to do this automatically.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject allergy-template/lein-template "0.1.0-SNAPSHOT"
-  :description "Created with lein-create-template"
-  :url "http://example.com/FIXME"
+(defproject allergy-template/lein-template "0.1.0"
+  :description "Template for ClojureScript Reagent projects, with many added goodies from Chestnut and the Reagent samples"
+  :url "https://github.com/deg/allergy-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
--- src/leiningen/new/allergy_template.clj
+++ src/leiningen/new/allergy_template.clj
@@ -8,26 +8,32 @@
   (let [data {:name name
               :ns-name (sanitize-ns name)
               :sanitized (name-to-path name)}]
-    (->files data ["src/cljs/{{sanitized}}/core.cljs" (render "core.cljs" data)]
-["env/prod/cljs/{{sanitized}}/prod.cljs" (render "prod.cljs" data)]
-["src/cljs/{{sanitized}}/reagent_helpers.cljs" (render "reagent_helpers.cljs" data)]
-["project.clj" (render "project.clj" data)]
-["env/dev/cljs/{{sanitized}}/dev.cljs" (render "dev.cljs" data)]
-["src/cljs/{{sanitized}}/session.cljs" (render "session.cljs" data)]
-["src/cljs/{{sanitized}}/views/new_page.cljs" (render "new_page.cljs" data)]
-["src/cljs/{{sanitized}}/views/app_page.cljs" (render "app_page.cljs" data)]
-["src/cljs/{{sanitized}}/views/home_page.cljs" (render "home_page.cljs" data)]
-["src/cljs/{{sanitized}}/views/common.cljs" (render "common.cljs" data)]
-["src/cljs/{{sanitized}}/views/dumper_page.cljs" (render "dumper_page.cljs" data)]
-["src/cljs/{{sanitized}}/routes.cljs" (render "routes.cljs" data)]
-["src/cljs/{{sanitized}}/views/pages.cljs" (render "pages.cljs" data)]
-["src/clj/{{sanitized}}/dev.clj" (render "dev.clj" data)]
-["src/clj/{{sanitized}}/server.clj" (render "server.clj" data)]
-["src/cljs/{{sanitized}}/views/user_page.cljs" (render "user_page.cljs" data)]
-["src/cljs/{{sanitized}}/views/about_page.cljs" (render "about_page.cljs" data)]
-["resources/public/css/json.human.css" (render "json.human.css")]
-["README.md" (render "README.md")]
-[".gitignore" (render ".gitignore")]
-["resources/index.html" (render "index.html")]
-["resources/public/css/style.css" (render "style.css")]
-)))
+    (->files data
+             [".gitignore" (render ".gitignore")]
+             ["README.md" (render "README.md")]
+             ["project.clj" (render "project.clj" data)]
+
+             ["env/dev/cljs/{{sanitized}}/dev.cljs" (render "env/dev/cljs/dev.cljs" data)]
+             ["env/prod/cljs/{{sanitized}}/prod.cljs" (render "env/prod/cljs/prod.cljs" data)]
+
+             ["resources/index.html" (render "resources/index.html")]
+
+             ["resources/public/css/json.human.css" (render "resources/public/css/json.human.css")]
+             ["resources/public/css/style.css" (render "resources/public/css/style.css")]
+
+             ["src/clj/{{sanitized}}/dev.clj" (render "src/clj/dev.clj" data)]
+             ["src/clj/{{sanitized}}/server.clj" (render "src/clj/server.clj" data)]
+
+             ["src/cljs/{{sanitized}}/core.cljs" (render "src/cljs/core.cljs" data)]
+             ["src/cljs/{{sanitized}}/reagent_helpers.cljs" (render "src/cljs/reagent_helpers.cljs" data)]
+             ["src/cljs/{{sanitized}}/routes.cljs" (render "src/cljs/routes.cljs" data)]
+             ["src/cljs/{{sanitized}}/session.cljs" (render "src/cljs/session.cljs" data)]
+
+             ["src/cljs/{{sanitized}}/views/about_page.cljs" (render "src/cljs/views/about_page.cljs" data)]
+             ["src/cljs/{{sanitized}}/views/app_page.cljs" (render "src/cljs/views/app_page.cljs" data)]
+             ["src/cljs/{{sanitized}}/views/common.cljs" (render "src/cljs/views/common.cljs" data)]
+             ["src/cljs/{{sanitized}}/views/dumper_page.cljs" (render "src/cljs/views/dumper_page.cljs" data)]
+             ["src/cljs/{{sanitized}}/views/home_page.cljs" (render "src/cljs/views/home_page.cljs" data)]
+             ["src/cljs/{{sanitized}}/views/new_page.cljs" (render "src/cljs/views/new_page.cljs" data)]
+             ["src/cljs/{{sanitized}}/views/pages.cljs" (render "src/cljs/views/pages.cljs" data)]
+             ["src/cljs/{{sanitized}}/views/user_page.cljs" (render "src/cljs/views/user_page.cljs" data)])))
