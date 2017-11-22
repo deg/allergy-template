058b25ae862ee6a84b25387674305f46362de4aa
David Goldfarb <deg@degel.com>
2017-11-22 17:28:14 +0200

    Dangling comments (from Jan15)

--- project.clj
+++ project.clj
@@ -1,5 +1,17 @@
+;;; Copyright (c) 2014 David Goldfarb. All rights reserved.
+;;; Contact info: deg@degel.com
+;;;
+;;; The use and distribution terms for this software are covered by the Eclipse
+;;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can
+;;; be found in the file epl-v10.html at the root of this distribution.
+;;; By using this software in any fashion, you are agreeing to be bound by the
+;;; terms of this license.
+;;;
+;;; You must not remove this notice, or any other, from this software.
+
+
 (defproject allergy-template/lein-template "0.1.0"
-  :description "Template for ClojureScript Reagent projects, with many added goodies from Chestnut and the Reagent samples"
+  :description "Template for ClojureScript Reagent projects, with many added goodies from Chestnut and the Reagent samples."
   :url "https://github.com/deg/allergy-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
